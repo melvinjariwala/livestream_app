a27ab8a1414e11e0011f99e2324fffc2eadf7960
melvinjariwala <melvinjariwala7.MJ@gmail.com>
2024-02-05 18:11:01 +0530

    Custom Overlay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [overlays, setOverlays] = useState([]);
-  const [newOverlay, setNewOverlay] = useState({
-    content: "",
-    position: { top: 10, left: 10 },
-    size: { width: 20, height: 10 },
-  });
+  const [content, setContent] = useState("");
+  const [top, setTop] = useState(10);
+  const [left, setLeft] = useState(10);
+  const [width, setWidth] = useState(20);
+  const [height, setHeight] = useState(10);
+  const [setSelectedOverlayId, setsetSelectedOverlayId] = useState("");
 
   useEffect(() => {
     fetchOverlays();
@@ -20,19 +21,23 @@ function App() {
         headers: { "Content-type": "application/json" },
       })
         .then((response) =>
-          response
-            .json()
-            .then((data) => console.log("overlays : ", data.overlays))
+          response.json().then((data) => setOverlays(data.overlays))
         )
         .catch((error) => console.log("error : ", error));
-
-      console.log("inside");
     } catch (error) {
-      console.log("Error fetching overlays!");
+      console.error("Error fetching overlays!", error);
     }
   };
 
-  const addOverlay = async () => {
+  const addCustomOverlay = async () => {
+    const newOverlay = {
+      content,
+      top,
+      left,
+      width,
+      height,
+    };
+
     try {
       const response = await fetch("/overlays", {
         method: "POST",
@@ -48,29 +53,57 @@ function App() {
     }
   };
 
-  const updateOverlay = async (id) => {
+  const selectOverlay = (overlay) => {
+    setContent(overlay.content);
+    setTop(overlay.top);
+    setLeft(overlay.left);
+    setWidth(overlay.width);
+    setHeight(overlay.height);
+    setsetSelectedOverlayId(overlay._id);
+  };
+
+  const resetForm = () => {
+    setContent("");
+    setTop(10);
+    setLeft(10);
+    setWidth(20);
+    setHeight(10);
+    setSelectedOverlayId(null);
+  };
+
+  const updateOverlay = async () => {
+    const updatedOverlay = {
+      content,
+      top,
+      left,
+      width,
+      height,
+    };
     try {
-      const response = await fetch(`/overlays/${id}`, {
+      const response = await fetch(`/overlays/${setSelectedOverlayId.$oid}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(newOverlay),
+        body: JSON.stringify(updatedOverlay),
       });
       await response.json();
       fetchOverlays();
+
+      resetForm();
     } catch (error) {
       console.error("Error updating overlay:", error);
     }
   };
 
-  const deleteOverlay = async (id) => {
+  const deleteOverlay = async () => {
     try {
-      const response = await fetch(`/overlays/${id}`, {
+      const response = await fetch(`/overlays/${setSelectedOverlayId.$oid}`, {
         method: "DELETE",
       });
       await response.json();
       fetchOverlays();
+      resetForm();
     } catch (error) {
       console.error("Error deleting overlay:", error);
     }
@@ -78,39 +111,103 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className="video-container">
-        <div>
-          <iframe
-            width="640"
-            height="480"
-            src="https://rtsp.me/embed/ZtTbt9at/"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
+      <div className="video-overlay-container">
+        <div className="video-container">
+          <div>
+            <iframe
+              title="LiveStream"
+              width="1280"
+              height="720"
+              src="https://rtsp.me/embed/ZtTbt9at/"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="overlay-container">
+          {overlays.map((overlay, index) => (
+            <div
+              key={index}
+              className="overlay"
+              style={{
+                top: `${overlay.top}px`,
+                left: `${overlay.left}px`,
+                width: `${overlay.width}px`,
+                height: `${overlay.height}px`,
+              }}
+              onClick={() => selectOverlay(overlay)}
+            >
+              {overlay.content}
+            </div>
+          ))}
         </div>
       </div>
-      <div className="overlay-container">
-        {console.log("overlays : ", overlays)}
-        {overlays.map((overlay) => (
-          <div
-            key={overlay._id}
-            className="overlay"
-            style={{
-              top: `${overlay.position.top}`,
-              left: `${overlay.position.left}`,
-              width: `${overlay.position.width}`,
-              height: `${overlay.position.height}`,
-            }}
+      <div className="overlay-form">
+        <h2>{setSelectedOverlayId ? "Update" : "Add"} custom overlay</h2>
+        <form>
+          <label>
+            Content:{" "}
+            <input
+              type="text"
+              name="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </label>
+          <label>
+            Top Position:{" "}
+            <input
+              type="number"
+              name="top"
+              value={top}
+              onChange={(e) => setTop(e.target.value)}
+            />
+          </label>
+          <label>
+            Left Position:{" "}
+            <input
+              type="number"
+              name="left"
+              value={left}
+              onChange={(e) => setLeft(e.target.value)}
+            />
+          </label>
+          <label>
+            Height:{" "}
+            <input
+              type="number"
+              name="height"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </label>
+          <label>
+            Width:{" "}
+            <input
+              type="number"
+              name="width"
+              value={width}
+              onChange={(e) => setWidth(e.target.value)}
+            />
+          </label>
+          <button
+            type="button"
+            onClick={setSelectedOverlayId ? updateOverlay : addCustomOverlay}
           >
-            {overlay.content}
-          </div>
-        ))}
-      </div>
-      <div className="controls-container">
-        <button onClick={() => fetchOverlays()}>Fetch Overlay</button>
-        <button onClick={() => addOverlay()}>Add Overlay</button>
-        <button onClick={() => updateOverlay()}>Update Overlay</button>
-        <button onClick={() => deleteOverlay()}>Delete Overlay</button>
+            {setSelectedOverlayId ? "Update" : "Add"} Overlay
+          </button>
+          {setSelectedOverlayId ? (
+            <button
+              type="button"
+              style={{ backgroundColor: "red" }}
+              onClick={deleteOverlay}
+            >
+              Delete Overlay
+            </button>
+          ) : (
+            <></>
+          )}
+        </form>
       </div>
     </div>
   );
