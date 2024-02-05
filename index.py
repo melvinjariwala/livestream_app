c65db04fe8186f3168995048b6a40aee1bf144c7
melvinjariwala <melvinjariwala7.MJ@gmail.com>
2024-02-05 22:51:13 +0530

    Changed PORT

--- index.py
+++ index.py
@@ -38,5 +38,5 @@ def delete_overlay(id):
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host= "127.0.0.1", port=10000)
+    serve(app, host= "127.0.0.1", port=5000)
     # app.run(debug=True)
